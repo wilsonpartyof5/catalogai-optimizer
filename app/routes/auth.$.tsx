3ffc6fe3d6ca2c383372e3ccf2f3ed6398f2a58c
mac <mac@DSW-Mac.local>
2025-10-08 14:19:45 -0400

    Add catch-all auth route to handle Shopify OAuth flow

---
+++ app/routes/auth.$.tsx
@@ -0,0 +1,9 @@
+import { authenticate } from "../shopify.server"
+import { type LoaderFunctionArgs } from "@remix-run/node"
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  await authenticate.admin(request)
+
+  return null
+}
+
